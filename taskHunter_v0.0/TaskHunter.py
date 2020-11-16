15dd13e0b8a3f1ecb4e73bcd46dacf9babd5a072
Gulbudak, Talha (IOT DS EU TR HMI&PN HMI) <talha.gulbudak@siemens.com>
2020-11-16 05:31:43 +0000

    v0.0 : implament random assigner func

--- taskHunter_v0.0/TaskHunter.py
+++ taskHunter_v0.0/TaskHunter.py
@@ -4,14 +4,77 @@
 import TaskModule
 import SprintModule
 import datetime
+import random
 
 class todoTask:
-    def __init__(self, id, name, date):
+    def __init__(self, id, date):
+        self.id = id
+        self.date = date
+        self.assignedID = -1
+
+class Person:
+    def __init__(self, id, name, capacity):
         self.id = id
         self.name = name
-        self.date = date
+        self.capacity = capacity
+        self.dayEffort = 0
+        self.sprintEffort = 0
+        self.actualEffort = 0
 
-def PrintTodoList(todoListIn):
+def GetPersonValue(peopleIn, id, varName):
+    for p in peopleIn:
+        if p.id == id:
+            return getattr(p, varName)
+    errorMessage = varName
+    errorMessage += " couldn't be found in people. ID : "
+    errorMessage += str(id)
+    return errorMessage
+
+def SetPersonValue(peopleIn, id, varName, val):
+    for p in peopleIn:
+        if p.id == id:
+            return setattr(p, varName, val)
+    errorMessage = varName
+    errorMessage += " couldn't be found in people. ID : "
+    errorMessage += str(id)
+    return errorMessage
+
+def CreateTodoList(todoListIn, tasksIn):
+    for task in tasksIn.taskList:
+        todoDate = task.lastDate + datetime.timedelta(days=task.period)
+        while True:
+            ## don't go on if todo date later than endDate
+            if todoDate >= sprintInfo.endDate:
+                break
+            
+            ## set start date to todo date if it is early than it
+            if todoDate < sprintInfo.startDate:
+                todoDate = sprintInfo.startDate
+
+            newtodo = todoTask(task.id, todoDate)
+            todoListIn.append(newtodo)
+            todoDate = todoDate + datetime.timedelta(days=task.period)
+
+def CalculateTodoEffort(todoListIn, tasksIn):
+    totalEffort = 0
+    for task in todoListIn:
+        totalEffort += tasksIn.GetElemValue(task.id, "effort")
+    return totalEffort
+
+def splitEffort(totalEffortIn, peopleIn, sprintInfoIn):
+    peoplecapacity = 0
+    sprintDays = int((sprintInfoIn.endDate - sprintInfoIn.startDate).days)
+    for person in peopleIn:
+        peoplecapacity += GetPersonValue(peopleIn, person.id, "capacity")
+
+    for person in peopleIn:
+        cap = GetPersonValue(peopleIn, person.id, "capacity")
+        sprintEff = (totalEffortIn*cap) / peoplecapacity
+        SetPersonValue(peopleIn, person.id, "spritEffort", sprintEff)
+        SetPersonValue(peopleIn, person.id, "dayEffort", sprintEff / sprintDays)
+        print(GetPersonValue(peopleIn, person.id, "name") , ":", GetPersonValue(peopleIn, person.id, "spritEffort"), ":", GetPersonValue(peopleIn, person.id, "dayEffort"))
+
+def PrintTodoList(todoListIn, tasksIn):
     print("----------------------------------------")
     print("Following tasks will done in this sprint")
     print("----------------------------------------")
@@ -19,9 +82,38 @@ def PrintTodoList(todoListIn):
     for todo in todoListIn:
         if not todo.id in printedTasks:
             taskCount = sum(t.id == todo.id for t in todoListIn)
-            print(todo.name, "-", taskCount, "times")
+            print(tasksIn.GetElemValue(todo.id, "name"), "-", taskCount, "times")
             printedTasks.append(todo.id)
     print("----------------------------------------")
+
+def RandomAssigner(todoListIn, peopleIn, sprintInfoIn, tasksIn):
+    sprintDays = int((sprintInfoIn.endDate - sprintInfoIn.startDate).days)
+    for day in range(sprintDays):
+        currentDay = sprintInfoIn.startDate + datetime.timedelta(days=day)
+        print("----------------------------------------")
+        print(currentDay.strftime("%d/%m/%Y %A"))
+        print("----------------------------------------")
+        for todo in todoListIn:
+            if todo.date != currentDay:
+                continue
+
+            taskEffort = tasksIn.GetElemValue(todo.id, "effort")
+            isTaskAssigned = False
+            for person in peopleIn:
+                if person.dayEffort * (day + 1) * 1.5 > person.actualEffort + taskEffort:
+                    todo.assignedID = person.id
+                    person.actualEffort += taskEffort
+                    peopleIn.sort(key=lambda x: x.actualEffort)
+                    isTaskAssigned = True
+                    print(tasksIn.GetElemValue(todo.id, "name"),"->\t", person.name)
+                    break
+            ## postpone task to next day if nobody assigned to it
+            if not isTaskAssigned:
+                todo.date = todo.date + datetime.timedelta(days=1)
+    print("----------------------------------------")
+    print("Total Efforts of people")
+    for person in peopleIn:
+        print(person.name,":",person.actualEffort)
 
 if __name__ == "__main__":
     print('*******************************************************')
@@ -41,19 +133,13 @@ if __name__ == "__main__":
 
     ## Create a todo list
     todoList = []
-    for task in tasks.taskList:
-        todoDate = task.lastDate + datetime.timedelta(days=task.period)
-        while True:
-            ## don't go on if todo date later than endDate
-            if todoDate >= sprintInfo.endDate:
-                break
-            
-            ## set start date to todo date if it is early than it
-            if todoDate < sprintInfo.startDate:
-                todoDate = sprintInfo.startDate
+    CreateTodoList(todoList, tasks)
+    PrintTodoList(todoList, tasks)
 
-            newtodo = todoTask(task.id, task.name, todoDate)
-            todoList.append(newtodo)
-            todoDate = todoDate + datetime.timedelta(days=task.period)
-    
-    PrintTodoList(todoList)            
+    people = [Person(0,"Talha",100), Person(1,"Seda",100)]
+
+    todoTotalEffort = CalculateTodoEffort(todoList, tasks)
+    splitEffort(todoTotalEffort, people, sprintInfo)
+    random.shuffle(people)
+
+    RandomAssigner(todoList, people, sprintInfo, tasks)
